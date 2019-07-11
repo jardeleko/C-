7a837570a0faab01d1dbad7840a222cae66fd951
Jardel O, Duarte <jardelduarte594@gmail.com>
2019-07-11 04:46:42 -0300

    metod private atl

--- POO/04.cpp
+++ POO/04.cpp
@@ -7,6 +7,8 @@ using namespace std;
 class lenguage{
 protected: 
 	char name[20];
+private:
+	int since;
 
 public:
 	void set_name(char* word){
@@ -16,17 +18,32 @@ public:
 	char* get_name(){
 		return this->name;
 	}
+
+	void set_since(int date){
+		this->since = date;
+	}
+
+	int get_since(){
+		return this->since;
+	}
 };
 
 
 int main (void){
 	char *ret, *word;
+	int date, am;
+
 	word = (char *)malloc(sizeof(20));
 	lenguage l;
 	cin >> word;
 	l.set_name(word);
 	ret = l.get_name();
-	cout << ret <<endl;
+	cout << ret <<endl;	
+	cin >> date;
+	l.set_since(date);
+	am = l.get_since();
+	cout << am << endl;
+
 
 	return 0;
 }	

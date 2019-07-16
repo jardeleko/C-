dba78bbcc935c52d340aeb4e18877be73bae0921
Jardel O, Duarte <jardelduarte594@gmail.com>
2019-07-16 06:49:38 -0300

    simple graph, first exemple + img

--- POO/TAD/05.cpp
+++ POO/TAD/05.cpp
@@ -17,6 +17,14 @@ bool is_linked(int i, int j){
 
 int main(void){
 
-	cout << is_linked(0, 2) << endl;
+	cout << is_linked(0, 2) << endl; // se A e C == 1 return true
 	return 0;
 }
+/* 
+
+	*A      D* //		  A B C D E 
+	  '    '			A 0 0 1 0 0
+	  	-C-				B 0 0 1 0 0
+	   '   '			C 1 1 0 1 1 
+	*B'     'E*   		D 0 0 1 0 1
+*/				//		E 0 0 1 1 0		
